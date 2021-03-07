ad7506d112dd63be3820fa845e332da54d5ab497
pubranko <mikuras3@outlook.com>
2021-03-07 22:56:38 +0900

    各メニューの操作性改良。 グループ化追加の処理を検討中。

---
+++ app/static/ts/etc/_menu_off.ts
@@ -0,0 +1,21 @@
+import { global_runing_events } from '../global/_global';    //グローバル変数
+
+/**
+ * 選択されたメニューリスト以外を全て非表示にする。
+ * @param key なし
+ */
+export const menu_off = (): void => {
+    //メニューリスト全件取得
+    let operation_menu = document.querySelectorAll('.p-operation_menu__ul');
+
+    operation_menu.forEach(menu => {
+        //表示中、かつ、処理中のメニューリスト以外は非表示にする。
+        if (!menu.classList.contains('u-display--none') && global_runing_events.indexOf(menu.id) < 0) {
+            //if()
+            menu.classList.add('u-display--none');
+        }
+    });
+
+    global_runing_events.splice(0);
+}
+
